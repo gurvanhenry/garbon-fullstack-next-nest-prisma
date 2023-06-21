4badc045896fcbff27d4be5199d28998390496d3
Gurvan Henry <gurvan.henry@gmail.com>
2023-06-21 16:26:04 +0200

    Update next config to generate static file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   env: {
     API_URL: 'http://localhost:3000',
   },
--- frontend/src/pages/fetch-server-data-2/index.tsx
+++ frontend/src/pages/fetch-server-data-2/index.tsx
@@ -1,15 +1,21 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+// import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { Layout } from '@/components/Layout';
-import { Tag, Text } from '@chakra-ui/react';
-import { GetMessageResponse, apiGetMessage } from '@/lib/api';
+import { Badge, Tag, Text } from '@chakra-ui/react';
+// import { GetMessageResponse, apiGetMessage } from '@/lib/api';
 
-export default function FetchServerData2({
-  serverData,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+// export default function FetchServerData2({
+//   serverData,
+// }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+//   const message = serverData?.content;
+
+export default function FetchServerData2({ serverData }: { serverData: any }) {
   const message = serverData?.content;
 
   return (
     <Layout>
+      <Badge mb="4" colorScheme="red">
+        DISABLED - for static export
+      </Badge>
       <Text>Data 2 - with next getServerSideProps</Text>
       <Text>Message from server :</Text>
       {message ? (
@@ -25,9 +31,9 @@ export default function FetchServerData2({
   );
 }
 
-export const getServerSideProps: GetServerSideProps<{
-  serverData: GetMessageResponse | null;
-}> = async () => {
-  const result = await apiGetMessage();
-  return { props: { serverData: result } };
-};
+// export const getServerSideProps: GetServerSideProps<{
+//   serverData: GetMessageResponse | null;
+// }> = async () => {
+//   const result = await apiGetMessage();
+//   return { props: { serverData: result } };
+// };
